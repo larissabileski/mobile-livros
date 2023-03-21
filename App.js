aeddef5dc1dbd73d661f6ec3a0c4a6e0f7d1710d
larissabileski <laricristinabileski@gmail.com>
2023-03-21 16:53:53 -0300

    aula expositiva

--- App.js
+++ App.js
@@ -1,16 +1,40 @@
 import React from 'react';
-import {View, Image, StyleSheet} from 'react-native';
+import {View, Image, Text, StyleSheet} from 'react-native';
 
-const DisplayAnImage = () => {
+function Card(props){
+  return (
+    <View style= {styles.card}>
+      <Image style={styles.livro} source={{ uri: props.livro.capa }}/>
+      <Text style={{ color: 'darkred'}}> {props.livro.titulo} </Text>
+      <Text style={{ fontSize: 12}}> R$ {props.livro.preco} </Text>
+    </View>
+  )
+}
+ export default function App() {
+  const livros = [
+    {
+      capa:'https://www.adobe.com/br/express/create/cover/media_19a7c3bcd464c0a9a955b9a538fe32f888e89c7e1.jpeg?width=400&format=jpeg&optimize=medium',
+      titulo: 'Livro 1',
+      preco: 254.90
+    },
+    {
+      capa:'https://www.adobe.com/br/express/create/cover/media_19a7c3bcd464c0a9a955b9a538fe32f888e89c7e1.jpeg?width=400&format=jpeg&optimize=medium',
+      titulo: 'Livro 2',
+      preco: 100.90
+    },
+    {
+      capa:'https://www.adobe.com/br/express/create/cover/media_19a7c3bcd464c0a9a955b9a538fe32f888e89c7e1.jpeg?width=400&format=jpeg&optimize=medium',
+      titulo: 'Livro 3',
+      preco: 50.90
+    },
+  ]
   return (
     <View style={styles.container}>
-      <Image
-        style={styles.stretch}
-        source={require('./img/capa.jpg')}
-      />
+      <Text style={{ fontSize: 25, fontWeight: 'bold'}}> Olá Mundo </Text>
+      <View style = {styles.conteudo}>{ livros.map(livro => <Card livro={livro} />)}</View>
     </View>
-  );
-};
+  )
+ }
 
 const styles = StyleSheet.create({
   container: {
